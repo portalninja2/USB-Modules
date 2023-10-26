dd1c64c37184398ced146dbb969287570a99f164
Dennis Brößner <65617101+portalninja2@users.noreply.github.com>
2023-10-26 13:17:57 +0200

    Create backup.bat

---
+++ backup.bat
@@ -0,0 +1,52 @@
+@echo off
+:backupmenue
+@title BackUP Tool
+cls
+echo BackUP Aktionen:
+echo =====================
+echo [c] Erstellen eines lokalen BackUP's (Diese Instanz)
+echo [r] BackUP von einem Link erstellen
+echo [m] BackUP von einem anderen Datenträger erstellen
+echo [g] BackUP wiederherstellen (lokal)
+echo [gr] BackUP wiederherstellen (Remote)
+
+echo.
+
+set basw=0
+set /p basw="Triff eine Auswahl: "
+
+if %basw%==c goto lcbackup
+if %basw%==r goto rcbackup
+if %basw%==g goto lwbackup
+if %basw%==gr goto rwbackup
+if %basw%==back goto END
+
+:lcbackup
+
+echo Beta!
+set name=0
+set /p name="Trage deinen Zusatz Namen ein: "
+set "name=LocalUSB-%name%.zip"
+set "files=apps Bilder Dokumente Musik	Scripts	Videos	Zip's USB-Modules.bat Setup.bat filesystem_script.bat"
+tar -c -f BackUP's\local\%name% %files%
+
+pause
+goto backupmenue
+
+:rcbackup
+echo coming soon
+pause
+goto backupmenue
+
+:lwbackup
+echo coming soon
+pause
+goto backupmenue
+
+:rwbackup
+echo coming soon
+pause
+goto backupmenue
+
+:END
+exit

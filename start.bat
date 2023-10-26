ad88d4042b6bb13a37f8158be35742f6c0cfc618
Dennis Brößner <65617101+portalninja2@users.noreply.github.com>
2023-10-26 13:25:10 +0200

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,1 +1,32 @@
+@echo off
+:Auswahlmenue
+@title USB-Stick-Menue
+cls
+echo.
+echo Treffe eine Auswahl
+echo =================================
+echo.
+if exist Scripts\modules\backup.bat echo [B]BackUP Manager
+echo [g]Get Modules
+echo.
+
+set asw=0
+set /p asw="Bitte eine Auswahl treffen: "
+
+if exist Scripts\modules\backup.bat if %asw%==b start Scripts\modules\backup.bat
+if %asw%==g goto getModules
+if %asw%==exit goto END
+
+goto Auswahlmenue
+
+:getModules
+curl --output modules.bat https://raw.githubusercontent.com/portalninja2/USB-Modules/main/get-modules.bat --ssl-no-revoke
+start modules.bat
+pause >nul
+
+:END
+echo bye
+pause >nul
+exit
+
 

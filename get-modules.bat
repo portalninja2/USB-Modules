b003baf33781d58f226f41f831e2ac0e006dfb03
Dennis Brößner <65617101+portalninja2@users.noreply.github.com>
2023-10-26 13:04:28 +0200

    Create get-modules.bat

---
+++ get-modules.bat
@@ -0,0 +1,31 @@
+@echo off
+@title GetModules
+:getModules
+echo.
+echo Treffe eine Auswahl
+echo =================================
+echo.
+echo [B]BackUP - Modul
+echo [PI]Programm Installazion - Modul
+echo [I]ISO Download - Modul
+echo [P]Ping - Modul
+echo [G]GitClone - Modul
+echo [S]Shutdown - Modul
+echo.
+
+set mdasw=0
+set /p mdasw="Bitte trffe eine Auswahl: "
+
+if %mdasw%==b goto BackUP
+if %mdasw%==pi goto programm
+if %mdasw%==i goto ISO
+if %mdasw%==p goto ping
+if %mdasw%==g goto gitclone 
+if %mdasw%==s goto shutdown 
+if %mdasw%==back goto Auswahlmenue
+
+
+:END
+echo bye
+pause >nul
+exit

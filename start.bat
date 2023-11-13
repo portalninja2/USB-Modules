2e53e8bd39ddfda4f684843aa97285ca5dfa30bf
Dennis Brößner <65617101+portalninja2@users.noreply.github.com>
2023-11-13 12:18:33 +0100

    Update start.bat

--- start.bat
+++ start.bat
@@ -19,9 +19,9 @@ set asw=0
 set /p asw="Bitte eine Auswahl treffen: "
 
 if exist Scripts\modules\backup.bat if %asw%==b goto BackUP
-if exist Scripts\modules\command-line.bat if %asw%==n goto command
+if exist Scripts\modules\command-line.bat if %asw%==c goto command
 if exist apps\Start.exe if %asw%==p goto portable
-if exist Scripts\modules\notes.bat if %asw%==c goto note
+if exist Scripts\modules\notes.bat if %asw%==n goto note
 if %asw%==g goto getModules
 if %asw%==exit goto END
 if %asw%==update goto update
@@ -49,7 +49,7 @@ start apps\Start.exe
 goto Auswahlmenue
 
 :note
-start Scripts\modules\command-line.bat
+start Scripts\modules\notes.bat
 goto END
 
 :command

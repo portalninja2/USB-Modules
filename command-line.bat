2f3218f67efd88931af4c8b7f0648b3337eb03f1
Dennis Brößner <65617101+portalninja2@users.noreply.github.com>
2024-01-05 20:21:20 +0100

    Update command-line.bat

--- command-line.bat
+++ command-line.bat
@@ -2,10 +2,72 @@
 @title command-line
 color 09
 cd Scripts\modules\
+echo.
+echo you can type "?" for help
+echo.
+
 :Command
+set asw=cls
+set /p asw=Gebe deinen Command ein: 
+if "%asw%" == "green" goto green
+if "%asw%" == "blue" goto blue
+if "%asw%" == "?" goto help
 
-set asw=0
-set /p asw="Gebe deinen Command ein: "
+if "%asw%" == "desktop" goto desktop
+if "%asw%" == "documents" goto documents
+if "%asw%" == "home" goto home
+if "%asw%" == "usb" goto usb
 
+
+
+:cmd
 %asw%
 goto Command
+
+:green
+color a
+goto Command
+
+:blue
+color 09
+goto Command
+
+:desktop
+cd /d %SystemDrive%\Users\%USERNAME%\Desktop
+goto Command
+
+:documents
+cd /d %SystemDrive%\Users\%USERNAME%\Documents
+goto Command
+
+:home
+cd /d %USERPROFILE%
+goto Command
+
+:usb
+set drive=D
+set /p drive="Drive: "
+cd /d "%drive%:\Scripts\modules"
+goto Command
+
+:help
+cls
+echo help
+echo =======================================================================
+echo.
+echo colors:
+echo - green
+echo - blue
+echo.
+echo paths
+echo.
+echo - desktop
+echo - documents
+echo - home
+echo - usb (goes to home)
+echo.
+echo others:
+echo - ? (help)
+
+goto Command
+
